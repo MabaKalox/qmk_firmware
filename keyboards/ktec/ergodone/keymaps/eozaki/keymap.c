d3dabc24cd405a1dc6dde64dc18feed83bc80fda
Nikita Romanov <NikitaR1@verifone.com>
2022-07-14 00:07:16 +0300

    ktec/ergodone: adjust eozaki layout for me

--- keyboards/ktec/ergodone/keymaps/eozaki/keymap.c
+++ keyboards/ktec/ergodone/keymaps/eozaki/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
              KC_QUOT,         KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_EQL,
              LSFT(KC_QUOT),  KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,              KC_MINS,
-                              KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_BSLS,
+                              KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_BSLS,
              KC_RBRC,         KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          TT(MDIA),
                                                  KC_DOWN,  KC_UP,KC_F6,  KC_F11,KC_RALT,
              KC_VOLD,        KC_VOLU,
