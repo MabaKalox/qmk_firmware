a2f546007d1911f82165cfd4685959ea0c059f6a
Nikita Romanov <NikitaR1@verifone.com>
2022-07-13 23:13:45 +0300

    ktec/ergodone: switch to qmk-dfu bootloader and enable some features of it

--- keyboards/ktec/ergodone/config.h
+++ keyboards/ktec/ergodone/config.h
@@ -6,3 +6,10 @@
 // TODO: handle matrix_size.cols and matrix_size.rows for custom matrix
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 14
+
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 6
+
+#define QMK_ESC_OUTPUT B4 // usually COL
+#define QMK_ESC_INPUT F4 // usually ROW
+#define QMK_LED B5
